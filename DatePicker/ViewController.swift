819941bb007b3e2cc01f1d91ca9a695a3b748576
NewSouthWave <tigers0110@naver.com>
2024-06-27 18:04:38 +0900

    알람기능구현

--- DatePicker/ViewController.swift
+++ DatePicker/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     let timeSelector: Selector = #selector(ViewController.updateTime)
     let interval = 1.0
     var count = 0
+    var alarmTime = ""
     
     @IBOutlet var lblCurrentTime: UILabel!
     @IBOutlet var lblPickerTime: UILabel!
@@ -28,20 +29,36 @@ class ViewController: UIViewController {
         let datePickerView = sender
         
         let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        
+        
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+
         lblPickerTime.text = "선택시간: " + formatter.string(from: datePickerView.date)
+        
+        formatter.dateFormat = "hh:mm aaa"
+        alarmTime = formatter.string(from: datePickerView.date)
         
     }
     
     @objc func updateTime(){ // objective C와의 호환성을 위해 @objc를
-//        lblCurrentTime.text = String(count)
-//        count += 1
+
         
         let date = NSDate()
         
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
         lblCurrentTime.text = "현재시간: " + formatter.string(from: date as Date)
+        
+        formatter.dateFormat = "hh:mm aaa"
+        let currentTime = formatter.string(for: date)
+        
+        if(alarmTime == currentTime) {
+            view.backgroundColor = UIColor.red
+        }
+        else{
+            view.backgroundColor = UIColor.white
+           
+        }
         
     }
 }
