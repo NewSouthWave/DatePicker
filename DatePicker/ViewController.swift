5ac7cd2f0946a1eb0da7429c837d0a64c56f174e
NewSouthWave <tigers0110@naver.com>
2024-06-27 17:02:24 +0900

    타이머 동작기능

--- DatePicker/ViewController.swift
+++ DatePicker/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let timeSelector: Selector = #selector(ViewController.updateTime)
+    let interval = 1.0
+    var count = 0
+    
+    @IBOutlet var lblCurrentTime: UILabel!
+    @IBOutlet var lblPickerTime: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
+        // 각 인자둘은 - 타이머간격 / 동작될 view / 타이머가 구동될 때 실행할 함수 / 사용자 정보 / 반복여부 를 나타냄
     }
 
-
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        let datePickerView = sender
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        lblPickerTime.text = "선택시간: " + formatter.string(from: datePickerView.date)
+        
+    }
+    
+    @objc func updateTime(){ // objective C와의 호환성을 위해 @objc를
+        lblCurrentTime.text = String(count)
+        count += 1
+    }
 }
 
